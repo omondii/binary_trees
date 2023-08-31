59da48f7402021482fa42a1f6be732d04ab060e9
Brian Omondi <omondiibrian00@gmail.com>
2023-09-01 01:33:56 +0300

    betty on task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,13 +11,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	else
 	{
 		if (tree)
 		{
-			l = tree->left ? 1 + binary_tree_height(tree->left): 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right): 0;
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((l > r) ? l:r);
+		return ((l > r) ? l : r);
 	}
 }
