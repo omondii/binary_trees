546933bd126e20ecc4ee8fb91bfbd8e0307e1c78
Brian Omondi <omondiibrian00@gmail.com>
2023-09-01 00:05:32 +0300

    count nodes and leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+  * binary_tree_leaves -  counts the leaves in a binary tree
+  * @tree: pointer to the root node of the tree to count the no of leaves
+  * Return: If tree is NULL, the function must return 0
+  */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	size_t left_leaves = binary_tree_leaves(tree->left);
+	size_t right_leaves = binary_tree_leaves(tree->right);
+
+	if (left_leaves == 0 && right_leaves == 0)
+		count++;
+
+	return (count + left_leaves + right_leaves);
+}
---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+  * @tree: pointer to the root node of the tree to count the no of nodes
+  * Return: count of nodes with at least i child
+  */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	size_t left_nodes = binary_tree_nodes(tree->left);
+	size_t right_nodes = binary_tree_nodes(tree->right);
+
+	if (tree->left || tree->right)
+		count++;
+	return (left_nodes + right_nodes + count);
+}
