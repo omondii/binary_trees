4da25d6a60d14b88db7ff6959804299ee82ce24c
Brian Omondi <omondiibrian00@gmail.com>
2023-08-31 23:38:21 +0300

    width, size

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+  * binary_tree_depth - calculate the depth of binary tree
+  * @tree: pointer to the node to measure the depth
+  * Return: depth of the tree
+  */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
