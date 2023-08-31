e987ad7e40cd3f018fbc04272c87d45cb6f11653
Brian Omondi <omondiibrian00@gmail.com>
2023-08-31 12:39:40 +0300

    Create node structure for a bt. Create, insert right & left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_left - inserts a node as the left-child f another node
+ *@parent: pointer to the node to insert the left-child in
+ *@value: the value to store in the new node
+ *Return: pointer to the created node (leftnode)
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *leftchild;
+
+	if (parent == NULL)
+	{
+		return NULL;
+	}
+
+	leftchild = binary_tree_node(parent, value);
+	if (leftchild == NULL)
+	{
+		return NULL;
+	}
+
+	if (parent->left != NULL)
+	{
+		leftchild->left = parent->left;
+		parent->left->parent = leftchild;
+	}
+	else
+	{
+		parent->left = leftchild;
+	}
+
+	return (parent);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_right - inserts a node as the right-child of another node
+ *@parent: pointer to the node to insert the right-child in
+ *@value: value to store in the new node
+ *Return: Newly created node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *rightchild;
+
+	if (parent == NULL)
+	{
+		return NULL;
+	}
+
+	rightchild = binary_tree_node(parent, value);
+	if (parent->right != NULL)
+	{
+		rightchild->right = parent->right;
+		parent->right->parent = rightchild;
+	}
+	parent->right = rightchild;
+	return (parent);
+}
